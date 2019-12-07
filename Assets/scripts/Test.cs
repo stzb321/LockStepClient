eed7521a431a5a9537bf56791ca7fe8f0d5335c4
oldman <445471528@qq.com>
2019-12-07 17:39:42 +0800

    first init

---
+++ Assets/scripts/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.scripts;
+
+public class Test : MonoBehaviour
+{
+    private NetworkProxy socket;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        socket = new TcpSocket();
+        socket.StartSocket();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
